2c1245edd8291d3a1c86845e43487bc80c6c9b04
George Nishimura <thetmkay@gmail.com>
2014-10-19 06:22:10 +0100

    Added time conversion

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,23 @@
 
 	'use strict';
       $(document).ready(function() {
+
+        function msToTime(s) {
+          var ms = s % 1000;
+          s = (s - ms) / 1000;
+          var secs = s % 60;
+          s = (s - secs) / 60;
+          var mins = s % 60;
+          var hrs = (s - mins) / 60;
+
+          return hrs + ':' + mins + ':' + secs + '.' + ms;
+        }
+
+        $(".timer-time").each(function(index, element) {
+          var $this = $(element);
+          $this.text(msToTime($this.text()));
+        });
+
         $("#app-trigger").click(function() {
           if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
             window.location = "stacktimer://add.timer?{{encoded_url}}"
