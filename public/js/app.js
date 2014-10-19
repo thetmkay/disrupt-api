2141207845184feb8029215cf80a65ed156d234a
George Nishimura <thetmkay@gmail.com>
2014-10-19 02:50:19 +0100

    added pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,14 @@
 (function($) {
 
 	'use strict';
+      $(document).ready(function() {
+        $("#app-trigger").click(function() {
+          if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+            window.location = "stacktimer://add.timer?{{encoded_url}}"
+          } else {
+            window.location = "http://stacktimer.com";
+          }
+        });
 
+      });
 })(jQuery);
