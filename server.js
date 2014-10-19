97aaaafa475ee073bd55c1f1adfcb1152d80e0a9
George Nishimura <thetmkay@gmail.com>
2014-10-19 01:10:37 +0100

    add parse

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var express = require('express'),
   http = require('http'),
   path = require('path');
 
+var Parse = require('parse').Parse;
+
+Parse.initialize("TpudbmSzk74NMI1GBDfs4FJq4E5Tk4MbJ4kgb5rR", "WA4rrkLGN1Pcx2RiyccEBrAm6h0DAY5hiYXXa9M4");
+
 var app = module.exports = express();
 
 
@@ -28,8 +32,22 @@ app.set('view engine', 'html');
  * Routes
  */
 
-app.get('/timer/:timer', function(req,res) {
-  res.render('add',req.params.timer);
+app.get('/timer/:routine', function(req,res) {
+  console.log(req.params.routine);
+  var query = new Parse.Query(Parse.Object.extend("Routine") );
+  query.get(req.params.routine, {
+    success: function(datastring) {
+      var data = datastring._serverData;
+      // console.log(data);
+      var user = data.user;
+      delete data.user;
+      // console.log(user);
+      // console.log(data.attributes.timers);
+      var encoded_url = encodeURI(JSON.stringify(data));
+      // console.log(encoded_url);
+      res.render('add', {user:user, name:data.name, timers:data.timers, encoded_url:encoded_url});
+    }
+  });
 });
 // redirect all others to the index (HTML5 history)
 app.get('/', function(req,res) {
